47747d6869e085f79e576b0b22b063e39593cafc
Kheijhackt <kheijhackt@gmail.com>
2026-03-09 17:30:32 +0800

    Updated resume

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -2,7 +2,7 @@ import { Typography, Button, Stack } from "@mui/material";
 import SectionWrapper from "../components/SectionWrapper";
 
 export default function Home() {
-  const RESUME_URL = "/kian-tubalinal-resume-01092026.pdf";
+  const RESUME_URL = "/kian-tubalinal-resume-03092026.pdf";
   const scrollTo = (id) => {
     document.getElementById(id).scrollIntoView({ behavior: "smooth" });
   };
